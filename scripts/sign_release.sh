e5d78998e8a63d4677f9b00d3394273491e01099
Luzifer-Black <passivearning388@gmail.com>
2025-11-16 17:27:37 +0100

    Delete scripts directory

--- scripts/sign_release.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [ -z "${1:-}" ]; then echo "Usage: $0 file"; exit 2; fi
-FILE="$1"
-gpg --armor --output "${FILE}.asc" --detach-sign "$FILE"
-echo "Signed $FILE -> ${FILE}.asc"
--- scripts/verify_release.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-if [ $# -lt 2 ]; then echo "Usage: $0 file signature.asc"; exit 2; fi
-gpg --verify "$2" "$1"
